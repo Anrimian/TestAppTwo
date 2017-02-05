5bab672596f2607ed37d49b2b706e1e674fbcef9
Emirinay <emirinay@gmail.com>
2017-02-05 14:49:16 +0300

    fix endless list page calculation

--- app/src/main/java/com/testapptwo/utils/android/views/recyclerview/endlesslist/EndlessListUtils.java
+++ app/src/main/java/com/testapptwo/utils/android/views/recyclerview/endlesslist/EndlessListUtils.java
@@ -10,11 +10,7 @@ public class EndlessListUtils {
 
     public static int getNextPage(List contentList, int pageSize) {
         int size = contentList.size();
-        int page = size / pageSize;
-        int lastPageCount = size % 20;
-        if (lastPageCount != 0 && page != 0) {
-            page++;
-        }
-        return page;
+        int pages = size / pageSize;
+        return ++pages;
     }
 }
